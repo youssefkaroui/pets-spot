cbbd3dde370e52162fe83268fff8af79b3d22d61
Lucas Young <48228936+uwlryoung@users.noreply.github.com>
2023-10-24 12:47:50 -0400

    Added a landing page and a footer

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, Box, Text, Flex } from "@chakra-ui/react";
+import { Link, Box, Image } from "@chakra-ui/react";
 // import { Link } from 'react-router-dom';
 // import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
 // import SignUpForm from './SignupForm';
@@ -12,12 +12,40 @@ const Navbar = () => {
   // const [showModal, setShowModal] = useState(false);
   return (
     <>
-      <Box display="flex" alignItems="center" justifyContent="right" bgColor='#34656D' h={100} w='100%'>
-        <Link className="nav-link" href="/">Home</Link>
-        <Link className="nav-link" href="/listings">Listings</Link>
-        <Link className="nav-link" href="/dashboard">Dashboard</Link>
-        <Link className="nav-link" href="/create">Create Listing</Link>
-        <Link className="nav-link" href="/login">Login / Signup</Link>
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+        bgColor="#34656D"
+        h={100}
+        w="100%"
+      >
+        <Box display="flex" p="10px">
+          <Image
+            mr="10px"
+            boxSize={80}
+            objectFit="cover"
+            borderRadius={300}
+            src="https://media.istockphoto.com/id/649091176/photo/puppy-and-kitten-closeup-over-white.jpg?s=612x612&w=0&k=20&c=WI6HA8rhU7bRZvNVlBOgjmCkKQr2SYRmqTcl3x6IeAY="
+            alt="cat-and-dog"
+          />
+          <h1>Pet Spot</h1>
+        </Box>
+        <Box>
+          <Link className="nav-link" href="/">
+            Home
+          </Link>
+          {/* <Link className="nav-link" href="/listings">Listings</Link> */}
+          <Link className="nav-link" href="/dashboard">
+            Dashboard
+          </Link>
+          <Link className="nav-link" href="/create">
+            Create Listing
+          </Link>
+          <Link className="nav-link" href="/login">
+            Login / Signup
+          </Link>
+        </Box>
       </Box>
     </>
   );
