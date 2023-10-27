d09b0690291e0e232110337c534b16caa1a4321c
Youssef <youssefkaroui6@gmail.com>
2023-10-27 15:30:42 -0400

    added an instance of mongoose.connect() to connection.js and added the closing bracket to fix bug

--- server/config/connections.js
+++ server/config/connections.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pet-spot');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/petsdb');
 
 module.exports = mongoose.connection;
