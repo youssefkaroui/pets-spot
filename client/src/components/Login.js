930154d16f5b94cba6845143c5155ca4d1c05af3
Youssef <youssefkaroui6@gmail.com>
2023-10-30 21:05:18 -0400

    added the logic for loginform

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,5 @@
-import React from "react";
 
+import React, { useState} from 'react';
 import {
   Modal,
   ModalOverlay,
@@ -16,10 +16,52 @@ import {
 } from "@chakra-ui/react";
 import { DefaultContext } from "react-icons/lib";
 
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../utils/mutations";
+import Auth from "../utils/auth";
+
+
 function LoginForm  () {
   const { isOpen, onOpen, onClose } = useDisclosure()
   const initialRef = React.useRef()
   const finalRef = React.useRef()
+
+  const [userFormData, setUserFormData] = useState({ email: '', password: '' });
+  const [validated] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const [login, { error }] = useMutation(LOGIN_USER);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserFormData({ ...userFormData, [name]: value });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    // check if form has everything (as per react-bootstrap docs)
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    try {
+      const { data } = await login({
+        variables: { ...userFormData },
+      });
+      Auth.login(data.login.token);
+    } catch (err) {
+      console.error(err);
+      setShowAlert(true);
+    }
+
+    setUserFormData({
+      username: "",
+      email: "",
+      password: "",
+    });
+  };
 
   return (
     <>
@@ -33,7 +75,7 @@ function LoginForm  () {
 
             <FormControl mt={4}>
               <FormLabel> Password</FormLabel>
-              <Input placeholder='Password' />
+              <Input type="password" placeholder='Password' />
             </FormControl>
             
             <ModalFooter>
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,6 +1,5 @@
 import { gql } from '@apollo/client';
 
-
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
@@ -58,23 +57,6 @@ export const LOGIN_USER = gql`
   }
 `;
 
-// export const ADD_USER = gql`
-//   mutation addUser($username: String!, $email: String!, $password: String!, $address: UserAddress) {
-//     addUser(userInput: {
-//       username: $username,
-//       email: $email,
-//       password: $password,
-//       address: $address
-//     }) {
-//       token
-//       user {
-//         _id
-//         username
-//       }
-//     }
-//   }
-// `;
-
 // Mutation for adding a new user
 export const ADD_USER = gql`
   mutation addUser($userInput: userForm) {
@@ -88,30 +70,6 @@ export const ADD_USER = gql`
     }
   }
 `;
-
-// export const ADD_PET = gql`
-//   mutation addPet($petData: petData!) {
-//     addPet(petData: $petData) {
-//       _id
-//       name
-//       species
-//       age
-//       sex
-//       image
-//       breed
-//       temperament
-//       childFriendly
-//       description
-//       dateCreated
-//       active
-//       medicalHistory {
-//         allergies
-//         vaccinated
-//         spayedNeutered
-//       }
-//     }
-//   }
-// `;
 
 // Mutation for adding a new pet
 export const ADD_PET = gql`
@@ -227,62 +185,3 @@ export const REMOVE_PET = gql`
     }
   }
 `;
-
-
-//followPet
-
-
-
-
-// export const UPDATE_USER = gql`
-//   mutation updateUser($userForm: userForm!, $password: String!) {
-//     updateUser(userForm: $userForm, password: $password) {
-//       _id
-//       username
-//       email
-//       address {
-//         line_one
-//         line_two
-//         city
-//         zipcode
-//       }
-//     }
-// }
-// `;
-
-// export const UPDATE_PET = gql`
-//   mutation updatePet($petData: petData!) {
-//     updatePet(petData: $petData) {
-//       _id
-//       name
-//       species
-//       age
-//       sex
-//       image
-//       breed
-//       temperament
-//       childFriendly
-//       description
-//       dateCreated
-//       active
-//       medicalHistory {
-//         allergies
-//         vaccinated
-//         spayedNeutered
-//       }
-//     }
-//   }
-// `;
-
-// export const REMOVE_PET = gql`
-//   mutation removePet($petId: ID!) {
-//     deletePet(petId: $petId) {
-//       _id
-//       name
-//       species
-//       # Include other fields you want to retrieve
-//     }
-//   }
-// `;
-
-
